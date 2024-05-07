239ec45ac9ffe3daf88cecc1df75aefbbd495ac4
Zraquera21 <jihanfebry2102@gmail.com>
2024-05-07 09:18:43 +0700

    VueJs-FrontEnd

---
+++ compro-wikrama-pertemuan-4/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navbar></Navbar>
+  <router-view></router-view>
+</template>
+<script>
+import Navbar from '@/components/Layouts/Navbar.vue';
+export default {
+  components: {
+    Navbar,
+  }
+}
+</script>
+
+<style>
+html{
+  font-family: 'Raleway', sans-serif;
+}
+</style>
---
+++ compro-wikrama-pertemuan-4/src/pages/Blog.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <Blog :data="DataBlog">
+            <div class="search">
+                <input type="text" class="searchTerm" placeholder="Cari Judul"  @keyup="handleSearch">
+            </div>
+        </Blog>
+    </div>
+</template>
+
+<script>
+    import { Get } from '@/Api/index.js';
+    import Blog from '@/components/Beranda/Blog.vue';
+    export default{
+        components:{
+            Blog
+        },
+        data(){
+            return{
+                DataBlog: []
+            }
+        },
+        async created(){
+            const response = await Get('http://127.0.0.1:9000/api/blog');
+            this.DataBlog = response.data.data;
+        }
+    }
+</script>
+
+<style>
+
+.search{
+    padding-bottom: 90px;
+}
+
+.searchTerm {
+    width: 300px; 
+    height: 30px; 
+    border: none;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    padding: 5px;
+}
+
+</style>
